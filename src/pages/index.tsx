37acba879e702c9a9b22575234097f1c43bc3a7d
Tanishq Singh <tanishqsingh640@gmail.com>
2022-10-20 03:25:42 +0530

    :zap: feat: noise with image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,10 @@ import Noise from "@/components/noise";
 
 const Home = () => {
     return (
-        <div className="min-h-visible h-full w-full relative bg-[#163a45]">
-            <main className="h-full w-full flex justify-center items-center">
-                <span className="text-[#dbe1c7] text-4xl tracking-widest font-bold underline underline-offset-4">NOISE</span>
-            </main>
-
-            <Noise />
+        <div className="min-h-visible h-full w-full">
+            <main className="h-full w-full flex justify-center items-center bg-neutral-100">
+            	<Noise />
+			</main>
         </div>
     )
 }
